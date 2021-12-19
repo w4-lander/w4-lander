cf1d640834dd9cd4a55a82c998484fdfeb432f9f
Eric Zhang <ekzhang1@gmail.com>
2021-12-19 00:05:08 -0600

    Add support for multiple doge objects

--- src/doge_meteor.zig
+++ src/doge_meteor.zig
@@ -1,4 +1,6 @@
-/// This feature creates a 16x16 pixel doge "meteor" object which follows the ship around in the game.
+//! This feature creates a 16x16 pixel doge "meteor" object which follows the ship
+//! around in the game.
+
 const w4 = @import("wasm4.zig");
 const math = @import("std").math;
 const lander = @import("lander.zig");
@@ -6,59 +8,49 @@ const utils = @import("utils.zig");
 const Point = utils.Point;
 
 /// Struct for doge object
-const Doge = struct {
+pub const Doge = struct {
     pos: Point,
 
     /// Initialize starting position coordinates
-    pub fn init(x: f32, y: f32) @This() {
-        return @This(){
+    pub fn init(x: f32, y: f32) Doge {
+        return Doge{
             .pos = Point{ .x = x, .y = y },
         };
     }
 
     /// Moves doge up by one pixel
-    pub fn shift_up(this: *@This()) void {
-        if (this.pos.y == 0) {
-            this.pos.y = 160;
+    pub fn shift_up(self: *Doge) void {
+        if (self.pos.y == 0) {
+            self.pos.y = 160;
         } else {
-            this.pos.y -= 1;
+            self.pos.y -= 1;
         }
     }
 
     /// Moves doge towards target object by 1 unit (unit = pixel)
-    pub fn move_towards(this: *@This(), target_pos: Point) void {
-        var x_displ = target_pos.x - this.pos.x;
-        var y_displ = target_pos.y - this.pos.y;
+    pub fn move_towards(self: *Doge, target_pos: Point) void {
+        var x_displ = target_pos.x - self.pos.x;
+        var y_displ = target_pos.y - self.pos.y;
         var dx = x_displ / math.sqrt(x_displ * x_displ + y_displ * y_displ);
         var dy = y_displ / math.sqrt(x_displ * x_displ + y_displ * y_displ);
         utils.log("x displacement = {}", .{dx});
-        this.pos.x = this.pos.x + dx;
-        this.pos.y = this.pos.y + dy;
+        self.pos.x = self.pos.x + dx;
+        self.pos.y = self.pos.y + dy;
+    }
+
+    pub fn draw(self: *Doge) void {
+        const dogeWidth = 16;
+        const dogeHeight = 16;
+        const dogeSprite = [64]u8{ 0x00, 0x40, 0x01, 0x00, 0x01, 0xd0, 0x07, 0x40, 0x07, 0xf5, 0x5f, 0xd0, 0x06, 0xaa, 0xaa, 0x90, 0x18, 0x5a, 0xaa, 0x90, 0x18, 0x5a, 0x16, 0xa4, 0x1a, 0xaa, 0x16, 0xa4, 0x55, 0x5a, 0xaa, 0xa9, 0x65, 0x6a, 0xff, 0xa9, 0x75, 0x6b, 0xff, 0xa9, 0x7d, 0xff, 0xff, 0xe9, 0x1f, 0x55, 0x7f, 0xe9, 0x1f, 0xff, 0xff, 0xe4, 0x07, 0xff, 0xff, 0xa4, 0x01, 0xff, 0xfe, 0x90, 0x00, 0x55, 0x55, 0x40 };
+
+        w4.DRAW_COLORS.* = 0x3240;
+        w4.blit(
+            &dogeSprite,
+            @floatToInt(i32, self.pos.x),
+            @floatToInt(i32, self.pos.y),
+            dogeWidth,
+            dogeHeight,
+            w4.BLIT_2BPP,
+        );
     }
 };
-
-pub var doge: Doge = undefined;
-
-const dogeWidth = 16;
-const dogeHeight = 16;
-const dogeSprite = [64]u8{ 0x00, 0x40, 0x01, 0x00, 0x01, 0xd0, 0x07, 0x40, 0x07, 0xf5, 0x5f, 0xd0, 0x06, 0xaa, 0xaa, 0x90, 0x18, 0x5a, 0xaa, 0x90, 0x18, 0x5a, 0x16, 0xa4, 0x1a, 0xaa, 0x16, 0xa4, 0x55, 0x5a, 0xaa, 0xa9, 0x65, 0x6a, 0xff, 0xa9, 0x75, 0x6b, 0xff, 0xa9, 0x7d, 0xff, 0xff, 0xe9, 0x1f, 0x55, 0x7f, 0xe9, 0x1f, 0xff, 0xff, 0xe4, 0x07, 0xff, 0xff, 0xa4, 0x01, 0xff, 0xfe, 0x90, 0x00, 0x55, 0x55, 0x40 };
-
-pub fn start() void {
-    doge = Doge.init(120, 120);
-}
-
-pub fn update(frame: u32, ship_pos: Point) void {
-    if (@mod(frame, 10) == 0) {
-        doge.move_towards(ship_pos);
-    }
-
-    w4.DRAW_COLORS.* = 0x3240;
-    w4.blit(
-        &dogeSprite,
-        @floatToInt(i32, doge.pos.x),
-        @floatToInt(i32, doge.pos.y),
-        dogeWidth,
-        dogeHeight,
-        w4.BLIT_2BPP,
-    );
-}
--- src/main.zig
+++ src/main.zig
@@ -1,20 +1,31 @@
 const w4 = @import("wasm4.zig");
 const lander = @import("lander.zig");
-const doge = @import("doge_meteor.zig");
+const Doge = @import("doge_meteor.zig").Doge;
 const utils = @import("utils.zig");
 
 var frame: u32 = 0;
+var doges: [2]Doge = undefined;
 
 export fn start() void {
-    doge.start();
+    doges[0] = Doge.init(100, 100);
+    doges[1] = Doge.init(50, 100);
 }
 
 export fn update() void {
-    lander.landerUpdate();
-    doge.update(frame, utils.Point{ .x = lander.ship.x, .y = lander.ship.y });
-
     if (@mod(frame, 60) == 0) {
         utils.log("Current frame = {}", .{frame});
     }
+
+    lander.landerUpdate();
+    if (@mod(frame, 10) == 0) {
+        for (doges) |*doge| {
+            doge.move_towards(.{ .x = lander.ship.x, .y = lander.ship.y });
+        }
+    }
+
+    for (doges) |*doge| {
+        doge.draw();
+    }
+
     frame += 1;
 }
